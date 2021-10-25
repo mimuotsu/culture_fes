5e96a4f5d19aa64baf30e7db1756ab5152a56c16
mimuotsu <93106572+mimuotsu@users.noreply.github.com>
2021-10-25 11:59:46 +0900

    1st

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function() {
+	setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
+
+$(function() {
+	setTimeout(function(){
+		$('.opening p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.opening').fadeOut(1600);
+	},1800); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
+
+$(function() {
+	setTimeout(function(){
+		$('.opening-mono p').fadeIn(1600);
+	},800); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.opening-mono').fadeOut(500);
+	},5000); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
